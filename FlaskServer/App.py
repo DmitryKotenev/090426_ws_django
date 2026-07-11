36d661de992e5bf07d9267b36a49ceacc935521a
DmitryKotenev <dmitry_kotenev@mail.ru>
2026-07-11 17:53:13 +0300

    Keep going...

--- FlaskServer/App.py
+++ FlaskServer/App.py
@@ -4,29 +4,67 @@ from joblib.parallel import method
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 
+"""
 from flask import Flask, jsonify, request
+"""
 
+print('Import success')
+
+"""
 app = Flask(__name__)
+"""
 
 #load_models
 #le
-job_LE = pickle.load(open('../FlaskServer/models/job_LE.pkl', 'rb'))
-marital_LE = pickle.load(open('../FlaskServer/models/marital_LE.pkl', 'rb'))
-education_LE = pickle.load(open('../FlaskServer/models/education_LE.pkl', 'rb'))
-default_LE = pickle.load(open('../FlaskServer/models/default_LE.pkl', 'rb'))
-housing_LE = pickle.load(open('../FlaskServer/models/housing_LE.pkl', 'rb'))
-loan_LE	= pickle.load(open('../FlaskServer/models/loan_LE.pkl', 'rb'))
-contact_LE	= pickle.load(open('../FlaskServer/models/contact_LE.pkl', 'rb'))
-month_LE	= pickle.load(open('../FlaskServer/models/month_LE.pkl', 'rb'))
-poutcome_LE	= pickle.load(open('../FlaskServer/models/poutcome_LE.pkl', 'rb'))
+job_LE = pickle.load(open('../FlaskServer/Models/job_LE.pkl', 'rb'))
+marital_LE = pickle.load(open('../FlaskServer/Models/marital_LE.pkl', 'rb'))
+education_LE = pickle.load(open('../FlaskServer/Models/education_LE.pkl', 'rb'))
+default_LE = pickle.load(open('../FlaskServer/Models/default_LE.pkl', 'rb'))
+housing_LE = pickle.load(open('../FlaskServer/Models/housing_LE.pkl', 'rb'))
+loan_LE	= pickle.load(open('../FlaskServer/Models/loan_LE.pkl', 'rb'))
+contact_LE	= pickle.load(open('../FlaskServer/Models/contact_LE.pkl', 'rb'))
+month_LE	= pickle.load(open('../FlaskServer/Models/month_LE.pkl', 'rb'))
+poutcome_LE	= pickle.load(open('../FlaskServer/Models/poutcome_LE.pkl', 'rb'))
 #predict le
-y_LE = pickle.load(open('../FlaskServer/models/y_LE.pkl', 'rb'))
+y_LE = pickle.load(open('../FlaskServer/Models/y_LE.pkl', 'rb'))
 #scaler
-num_scaler = pickle.load(open('../FlaskServer/models/num_scaler.pkl', 'rb'))
+num_scaler = pickle.load(open('../FlaskServer/Models/num_scaler.pkl', 'rb'))
 #ML models
-kNN = pickle.load(open('../FlaskServer/models/kNN.pkl', 'rb'))
+kNN = pickle.load(open('../FlaskServer/Models/kNN.pkl', 'rb'))
+
+print('Models loaded successfully')
 
 #get_data
+
+##categorical
+X_cat_from_keyboard = ['unemployed',
+                       'married',
+                       'primary',
+                       'no',
+                       'no',
+                       'yes',
+                       'cellular',
+                       'may',
+                       'failure'
+                        ]
+
+X_nums_from_keyboard = [30,
+                        1787,
+                        16,
+                        199,
+                        4,
+                        330,
+                        0]
+
+le_list = [job_LE,
+           marital_LE,
+           education_LE,
+           default_LE,
+           housing_LE,
+           loan_LE,
+           contact_LE,
+           month_LE,
+           poutcome_LE]
 
 
 def predict(client_data) -> str:
@@ -36,9 +74,12 @@ def predict(client_data) -> str:
     :return: Результат yes/no
     """
     ##categorical
+    """
     X_cat_from_keyboard = client_data[0][0:9]
+    """
     print(X_cat_from_keyboard)
     # print(X_cat_from_keyboard)
+    """
     le_list = [job_LE,
                marital_LE,
                education_LE,
@@ -48,6 +89,7 @@ def predict(client_data) -> str:
                contact_LE,
                month_LE,
                poutcome_LE]
+    """
     X_le_list = [] #под закодированные признаки
     for i in range(len(X_cat_from_keyboard)):
         x_cat = le_list[i].transform([X_cat_from_keyboard[i]])[0]
@@ -56,7 +98,10 @@ def predict(client_data) -> str:
     print('X_cat_le:', X_le_list)
     ##num
 
+    """
     X_nums_from_keyboard = client_data[0][9:]
+    """
+
     print('X_nums', X_nums_from_keyboard)
     #объединить категориальные и числовые (в том же порядке, как и при обучении)
     X = []
@@ -73,6 +118,10 @@ def predict(client_data) -> str:
     result = y_LE.inverse_transform(prediction)[0]
     return result
 
+result = predict(0)
+print("prediction = ", predict(0))
+
+"""
 @app.route("/api/v1/get_data/", methods=["GET", "POST"])
 def get_data():
     client_data = request.json['client_form'] #получаем json от клиента
@@ -81,3 +130,6 @@ def get_data():
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+    
+
+"""
